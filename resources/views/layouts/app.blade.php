9947cb6b9c1206ea9c18f42f9f2b721ccc4d40a0
ongaikngee@gmail.com <ongaikngee@gmail.com>
2021-04-09 08:48:42 +0800

    commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>0409_recap</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}"> 
+</head>
+<body class="bg-gray-400">
+    <nav class="flex justify-between">
+        <ul class="flex">
+            <li><a href="" class="p-3">One</a></li>
+            <li><a href="" class="p-3">two</a></li>
+        </ul>
+        <ul class="flex">
+            <li><a href="" class="p-3">Apple</a></li>
+            <li><a href="" class="p-3">Orange</a></li>
+        </ul>
+    </nav>
+    @yield('content')
+</body>
+</html>
